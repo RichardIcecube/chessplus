85db21a72ad310dc792ba29fac1c26457abc33cd
RichardIcecube <Richardtheicecube@gmail.com>
2023-06-23 20:44:44 -0500

    Meter gain functions. Meter stack does not function yet

--- src/js/EXChess.js
+++ src/js/EXChess.js
@@ -5,9 +5,25 @@ import Meter from "./Meter.js";
 import GameOver from "./GameOver.js";
 import "../css/Chessboard.css";
 
+var p1meter = 0;
+var p2meter = 0;
+var p1stack = 0;
+var p2stack = 0;
+
 function EXChess() {
   var curPiece;
+
   const [game, setGame] = useState(new Chess());
+  const PAWN_METER_GAIN = 5;
+  const KNIGHT_METER_GAIN = 15;
+  const BISHOP_METER_GAIN = 15;
+  const ROOK_METER_GAIN = 20;
+  const QUEEN_METER_GAIN = 25;
+  const KING_METER_GAIN = 25;
+
+  const MAX_METER = 280;
+  const METER_STACK = 1;
+
   function onDrop(sourceSquare, targetSquare) {
     const move = makeAMove({
       from: sourceSquare,
@@ -20,17 +36,57 @@ function EXChess() {
 
   function makeAMove(move) {
     const gameCopy = new Chess(game.fen());
-    const result = buildMeter(move, gameCopy);
-    
+    const result = buildMeter(move, gameCopy);   
     setGame(gameCopy);
     return result; 
   }
 
   function buildMeter(move, gameCopy){
+    var builtMeter = 0;
     const sourcePiece = gameCopy.get(move.from);
 
     if(sourcePiece){
       curPiece = sourcePiece.type.charAt(0);
+      switch(curPiece){
+        case 'p':
+            builtMeter = PAWN_METER_GAIN;
+            break;
+        case 'n':
+            builtMeter = KNIGHT_METER_GAIN;
+            break;
+        case 'b':
+            builtMeter = BISHOP_METER_GAIN;
+            break;
+        case 'r':
+            builtMeter = ROOK_METER_GAIN;
+            break;
+        case 'q':
+            builtMeter = QUEEN_METER_GAIN;
+            break;
+        case 'k':
+            builtMeter = KING_METER_GAIN;
+            break;
+        default:
+            break;
+      }
+
+      if(game.turn() === 'b'){
+        p2meter += builtMeter;
+      }
+      else{
+        p1meter += builtMeter;
+      }
+
+      while(p1meter >= MAX_METER){
+        p1meter -= MAX_METER;  
+        p1stack += METER_STACK;
+      }
+
+      while(p2meter >= MAX_METER){
+        p2meter -= MAX_METER;  
+        p2stack += METER_STACK;
+      }
+
     }
     return gameCopy.move(move);
   }
@@ -44,7 +100,7 @@ function EXChess() {
             <h1 class="game-mode" id="ex">EX Chess</h1>
             <h1 class="turn-indicator">{game.turn() === 'b' ? "Black to Move" : "White to Move"}</h1>
             <Chessboard id="chessboard" position={game.fen()} onPieceDrop={onDrop} />
-            {Meter(curPiece, game.turn())}
+            {Meter(p1meter, p2meter, p1stack, p2stack)}
       </div>)}
     </div>
   );
--- src/js/Meter.js
+++ src/js/Meter.js
@@ -1,79 +1,13 @@
 import "../css/Meter.css";
 import React, { useRef, useEffect } from 'react';
-import { useState } from "react";
 
-var p1stack = 0;
-var p2stack = 0;
-
-const PAWN_METER_GAIN = 5;
-const KNIGHT_METER_GAIN = 15;
-const BISHOP_METER_GAIN = 15;
-const ROOK_METER_GAIN = 20;
-const QUEEN_METER_GAIN = 25;
-const KING_METER_GAIN = 25;
-
-const MAX_METER = 280;
-const METER_STACK = 1;
-export default function Meter(piece, turn) {   
-    var builtMeter;
-    const p1 = useRef(0);
-    const p2 = useRef(0);
-    const [p1meter, setP1meter] = useState(p1);
-    const [p2meter, setP2meter] = useState(p2);
-    const p1meterref = useRef(null);
-    const p2meterref = useRef(null);
-    switch(piece){
-        case 'p':
-            builtMeter = PAWN_METER_GAIN;
-            break;
-        case 'n':
-            builtMeter = KNIGHT_METER_GAIN;
-            break;
-        case 'b':
-            builtMeter = BISHOP_METER_GAIN;
-            break;
-        case 'r':
-            builtMeter = ROOK_METER_GAIN;
-            break;
-        case 'q':
-            builtMeter = QUEEN_METER_GAIN;
-            break;
-        case 'k':
-            builtMeter = KING_METER_GAIN;
-            break;
-        default:
-            break;
-    }
-    useEffect(() => {
-        if(turn === 'b'){
-            setP2meter(p2meter + builtMeter);
-            p2.current = p2meter;
-        }
-        else{
-            setP1meter(p1meter + builtMeter);
-            p1.current = p1meter;
-        }
-        while(p1meter >= MAX_METER){
-            setP1meter(p1meter - MAX_METER);
-            p1.current = p1meter;
-            p1stack += METER_STACK;
-        }
-        while(p2meter >= MAX_METER){
-            setP2meter(p2meter - MAX_METER);
-            p2.current = p2meter;
-            p2stack += METER_STACK;
-        }
-        if (p1meterref.current && p2meterref.current) {
-          p1meterref.current.style.width = `${p1meter}px`;
-          p2meterref.current.style.width = `${p2meter}px`;
-        }
-      }, [p1meter, p2meter]);
+export default function Meter(p1meter, p2meter, p1stack, p2stack) {   
     return(
         <div>
             <div class="meter" id="p1-meter"/>
             <div class="meter" id="p2-meter"/>
-            <div class="built-meter" id="p1-built-meter" ref={p1meterref}/>
-            <div class="built-meter" id="p2-built-meter" ref={p2meterref}/>
+            <div class="built-meter" id="p1-built-meter" style={{ width: `${p1meter}px` }}/>
+            <div class="built-meter" id="p2-built-meter" style={{ width: `${p2meter}px` }}/>
             <h1 class="meter-stack" id="p1-meter-stack">0</h1>
             <h1 class="meter-stack" id="p2-meter-stack">0</h1>
         </div>
